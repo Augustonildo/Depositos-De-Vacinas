e37bf08ec5b900ab5273b095c27709961863ba5a
augustonildo <augustocarvalhopp@gmail.com>
2021-09-06 21:24:52 -0300

    Estrutura inicial de arquivos

---
+++ src/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string tipoTarefa;
+    cin >> tipoTarefa;
+    int numeroVertices, numeroTrilhas;
+    cin >> numeroVertices >> numeroTrilhas;
+
+    return 0;
+}
