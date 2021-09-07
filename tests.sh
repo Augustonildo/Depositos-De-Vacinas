fee5680016f4e3b95a3a2bbb5bd49ed0b38d681b
augustonildo <augustocarvalhopp@gmail.com>
2021-09-07 15:59:21 -0300

    Casos de teste

--- tests.sh
+++ tests.sh
@@ -4,7 +4,7 @@ TMP_OUT=$2
 for i in {0..7..1}; do
   testname=$(printf "ct0%01d" $i)
   $EXEC < tests/$testname.txt > $TMP_OUT
-  if ! diff -qwB tests/$testname-output.txt $TMP_OUT &>/dev/null; then
+  if ! diff -qwB tests/out_$testname.txt $TMP_OUT &>/dev/null; then
     echo "Test $testname failed"
   else
     echo "Test $testname passed"
